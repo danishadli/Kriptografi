f6ec8407633ef335024352e9ad37f69801fd114d
kiakioo <ashfaashari@gmail.com>
2025-12-23 07:08:24 +0700

    baru lagi

--- app.py
+++ app.py
@@ -65,4 +65,5 @@ def export(preset):
     return send_file(path, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
